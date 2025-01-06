f822746093e2ec81045192c46b0888415d8ac4a5
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-06 15:32:55 +0530

    refactor: gave padding 5 in dashboard page

--- web/src/pages/admin/dashboard.tsx
+++ web/src/pages/admin/dashboard.tsx
@@ -1,6 +1,6 @@
 function Dashboard() {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-3 gap-4 p-5">
       <div className="rounded-3xl w-full h-[400px] bg-zinc-100 p-4"></div>
       <div className="rounded-3xl w-full h-[400px] bg-zinc-100 p-4"></div>
       <div className="rounded-3xl w-full h-[400px] bg-zinc-100 p-4 col-span-3"></div>
