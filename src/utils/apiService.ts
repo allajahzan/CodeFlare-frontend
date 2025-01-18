550ebe9a3312a6e0ee7c0cd16c7e9e10e04ff1d8
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-18 10:14:17 +0530

    feat: api service functions

---
+++ src/utils/apiService.ts
@@ -0,0 +1,79 @@
+import axiosInstance from "./axiosInstence";
+
+/**
+ * Fetches data from the given URL and returns the response.
+ * @param url The URL to make the GET request to.
+ * @returns The response data.
+ * @throws The error if the request fails.
+ */
+export const fetchData = async (url: string) => {
+    try {
+        const resp = await axiosInstance.get(url);
+        return resp.data;
+    } catch (err: any) {
+        throw err;
+    }
+};
+
+/**
+ * Posts data to the given URL and returns the response.
+ * @param url The URL to make the POST request to.
+ * @param data The data to be sent in the request body.
+ * @returns The response data.
+ * @throws The error if the request fails.
+ */
+export const postData = async (url: string, data?: any) => {
+    try {
+        const resp = await axiosInstance.post(url, data || null);
+        return resp.data;
+    } catch (err: any) {
+        throw err;
+    }
+};
+
+/**
+ * Performs a full update of the data at the given URL, replacing the existing data.
+ * @param url The URL to make the PUT request to.
+ * @param data The data to be sent in the request body.
+ * @returns The response data.
+ * @throws The error if the request fails.
+ */
+export const fullUpdateData = async (url: string, data?: any) => {
+    try {
+        const resp = await axiosInstance.put(url, data || null);
+        return resp.data;
+    } catch (err: any) {
+        throw err;
+    }
+};
+
+/**
+ * Performs a partial update of the data at the given URL, updating only the fields given in the request body.
+ * @param url The URL to make the PATCH request to.
+ * @param data The data to be sent in the request body.
+ * @returns The response data.
+ * @throws The error if the request fails.
+ */
+export const partialUpdateData = async (url: string, data?: any) => {
+    try {
+        const resp = await axiosInstance.patch(url, data || null);
+        return resp.data;
+    } catch (err: any) {
+        throw err;
+    }
+};
+
+/**
+ * Deletes the data at the given URL.
+ * @param url The URL to make the DELETE request to.
+ * @returns The response data.
+ * @throws The error if the request fails.
+ */
+export const deleteData = async (url: string) => {
+    try {
+        const resp = await axiosInstance.delete(url);
+        return resp.data;
+    } catch (err: any) {
+        throw err;
+    }
+};
