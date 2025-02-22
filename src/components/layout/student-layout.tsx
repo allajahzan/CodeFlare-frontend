8cea8e9e05691a3a1956fe27479e44031dc25853
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-02-22 20:59:38 +0530

    patch: restored the sidebar folder

--- src/components/layout/student-layout.tsx
+++ src/components/layout/student-layout.tsx
@@ -19,7 +19,7 @@ import Shadow from "@/components/ui/shadow";
 import { useLayoutEffect, useMemo } from "react";
 import { cn } from "@/lib/utils";
 import AppRoutes from "@/constants/app-routes";
-import Sidebar from "../common/sidebar/sidebar";
+import Sidebar from "../common/sidebar/sideBar";
 
 // Interface for Props
 interface PropsType {
