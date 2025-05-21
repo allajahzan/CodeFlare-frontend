0acf2b10a32e9572265887b96fb721ca98ad92b2
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-05-21 13:00:35 +0530

    patch: made some changes to text and media card

--- src/components/chat/media-card.tsx
+++ src/components/chat/media-card.tsx
@@ -2,7 +2,6 @@ import { cn } from "@/lib/utils";
 import { Message } from "./chat";
 import { CheckCheck, ChevronDown, Loader2 } from "lucide-react";
 import { useState, useRef } from "react";
-import { getRelativeTime } from "@/utils/relative-time";
 
 // Interface for Props
 interface PropsType {
@@ -55,33 +54,39 @@ function MediaCard({ msg, className }: PropsType) {
             )}
 
             {/* Image */}
-            <img
-                ref={imageRef}
-                className={cn(
-                    `w-[200px] md:w-[225px] h-[${imageHeight}px] object-cover rounded-lg transition-opacity duration-300`,
-                    loading ? "opacity-0" : "opacity-100"
-                )}
-                src={msg.message}
-                onLoad={handleImageLoad}
-                alt="media"
-            />
-
-            {/* Time and Status (after image loaded) */}
-            {!loading && (
-                <small className="w-full absolute p-2 px-3 right-0 bottom-0 flex items-center justify-end gap-1 text-[10px] text-white font-semibold">
-                    {getRelativeTime(msg.createdAt)}
-                    {msg.status === "sent" && (
-                        <CheckCheck className="w-4 h-4 text-blue-400" />
+            <div className="relative">
+                <img
+                    ref={imageRef}
+                    className={cn(
+                        `w-[200px] md:w-[225px] h-[${imageHeight}px] object-cover rounded-lg transition-opacity duration-300`,
+                        loading ? "opacity-0" : "opacity-100"
                     )}
-                </small>
-            )}
+                    src={msg.message}
+                    onLoad={handleImageLoad}
+                    alt="media"
+                />
 
-            {/* Options (hidden when image loading) */}
-            {!loading && (
-                <div className="absolute top-0 right-0 p-3 group-hover:visible invisible">
-                    <ChevronDown className="w-5 h-5 text-white" />
-                </div>
-            )}
+                {/* Time and Status (after image loaded) */}
+                {!loading && (
+                    <small className="w-full absolute group-hover:bg-black/20 rounded-b-lg p-1 px-3 right-0 bottom-0 flex items-center justify-end gap-1">
+                        <p className="relative z-10  text-[10px] text-white font-semibold">
+                            {new Date(msg.createdAt).toLocaleTimeString("en-US", {
+                                timeStyle: "short",
+                            })}
+                        </p>
+                        {msg.status === "sent" && (
+                            <CheckCheck className="w-4 h-4 text-blue-400 relative z-10" />
+                        )}
+                    </small>
+                )}
+
+                {/* Options (hidden when image loading) */}
+                {!loading && (
+                    <div className="absolute group-hover:bg-black/20 top-0 right-0 rounded-tr-lg rounded-bl-lg p-1 group-hover:visible invisible flex justify-end cursor-pointer">
+                        <ChevronDown className="w-4 h-4 text-white relative z-10" />
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- src/components/chat/text-card.tsx
+++ src/components/chat/text-card.tsx
@@ -1,7 +1,6 @@
 import { CheckCheck, ChevronDown } from "lucide-react";
 import { Message } from "./chat";
-import { cn } from "@/lib/utils"
-import { getRelativeTime } from "@/utils/relative-time";
+import { cn } from "@/lib/utils";
 
 // Interface for Props
 interface PropsType {
@@ -23,7 +22,9 @@ function TextCard({ msg, className }: PropsType) {
 
             {/* Time */}
             <small className="absolute right-2 bottom-0.5 flex items-center gap-1 text-[10px] text-muted-foreground font-medium">
-                {getRelativeTime(msg.createdAt)}
+                {new Date(msg.createdAt).toLocaleTimeString("en-US", {
+                    timeStyle: "short",
+                })}
                 {msg.status === "sent" && (
                     // <Check className="w-4 h-4 text-muted-foreground" />
                     <CheckCheck className="w-4 h-4 text-blue-400" />
